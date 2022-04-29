11b5b081b41a82c529ab2de9ebc9e17ba982befd
ihor dudkevich <egorka01051993@gmail.com>
2022-04-29 12:07:23 +0300

    form component

--- my-app/src/Components/Contact/Form/Form.tsx
+++ my-app/src/Components/Contact/Form/Form.tsx
@@ -1,16 +1,31 @@
-import React, {FC} from 'react';
+import React, {FC, FormEvent, useState, memo} from 'react';
 import clN from "./form.module.scss";
 
 const Form: FC = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handlerSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    }
+
+
     return (
         <form className={clN.form}>
 
-            <input className={clN.name} type={"text"} required={true} placeholder={'Name'}/>
-            <input className={clN.email} type={"email"} required={true} placeholder={'Email'}/>
-            <textarea className={clN.message} required={true} placeholder={"Message"}/>
-            <button className={clN.submit} type={"submit"}>Send</button>
+            <input onChange={(e) => {
+                setName(e.target.value)
+            }} className={clN.name} type={"text"} required={true} placeholder={'Name'}/>
+            <input onChange={(e) => {
+                setEmail(e.target.value)
+            }} className={clN.email} type={"email"} required={true} placeholder={'Email'}/>
+            <textarea onChange={(e) => {
+                setMessage(e.target.value)
+            }} className={clN.message} required={true} placeholder={"Message"}/>
+            <button onSubmit={handlerSubmit} className={clN.submit} type={"submit"}>Send</button>
         </form>
     );
 };
 
-export default Form;
+export default memo(Form);
